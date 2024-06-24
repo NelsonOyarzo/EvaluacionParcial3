3565a9c68c7f120c66e9b4ec4c064d395e436e98
NelsonOyarzo <168996751+NelsonOyarzo@users.noreply.github.com>
2024-06-24 09:14:05 -0400

    Update funciones.py
    Creación de la función agregar libro

--- funciones.py
+++ funciones.py
@@ -1,1 +1,16 @@
+libros=[]
 
+def agregar_libro():
+    titulo=input("Ingrese el nombre del libro que desea registrar: ")
+    autor=input("Ingrese el nombre del autor: ")
+    plublicacion=input("Ingrese el año de publicacion: ")
+    genero=input("Ingrese en genero del libro: ")
+    
+    libro={
+        "nombre":titulo,
+        "autor":autor,
+        "publicacion":plublicacion,
+        "genero":genero
+    }
+    libros.append(libro)
+    print("¡Se ha registrado de manera correcta el libro!")
