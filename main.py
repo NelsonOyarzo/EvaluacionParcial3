e613a5dfd92ec344cbf2658f8a7d94f1ee945901
matiasCaileo <ma.caileo@duocuc.cl>
2024-06-24 09:24:06 -0400

    Update main.py

--- main.py
+++ main.py
@@ -7,3 +7,7 @@ while True:
     print ("4 eliminar un libro ");
     print ("5 guardar coleccion en un libro");
     print ("6 salir del programa");
+    try:
+        opcion=int(input("** ingrese una opcion --> ")); 
+    except ValueError:
+        print("debe ser un numero del 1 al 6");
