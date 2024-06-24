c2ddb7c34407ae49c7f7c5a452ec368d03529730
Benitezdb <Die.benitez@duocuc.cl>
2024-06-24 09:23:37 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+import funciones as funcion
 
+while True:
+    print ("1 agregar libro");
+    print ("2 ver todos los libros");
+    print ("3 modificar un libro");
+    print ("4 eliminar un libro ");
+    print ("5 guardar coleccion en un libro");
+    print ("6 salir del programa");
